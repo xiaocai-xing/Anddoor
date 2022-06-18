ad27c4580a54158d3ef4ab7e70c939aa2989f559
cxq25802@foxmail.com <cxq2580@foxmail.com>
2022-06-18 18:04:25 +0800

    新增页面元素处理

--- src/main/java/com/wan/door/controller/WebDriverController.java
+++ src/main/java/com/wan/door/controller/WebDriverController.java
@@ -2,16 +2,21 @@ package com.wan.door.controller;
 
 import com.wan.door.common.ErrorInfo;
 
+import com.wan.door.common.ResponseInfo;
 import com.wan.door.common.ResponseInfotwo;
+import com.wan.door.dao.DelPage;
 import com.wan.door.dao.PageDao;
 import com.wan.door.dao.PageElement;
+import com.wan.door.data.User.UserDel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -55,5 +60,36 @@ public class WebDriverController {
     }
 
 
+    //获取页面列表接口
+    @RequestMapping("/pageeleList")
+    public ResponseInfo UserList(){
+        ResponseInfo result = new ResponseInfo();
+        List<Map<String, Object>> Pageinfo = pageService.QueryPageElementList();
+        result.setData(Pageinfo);
+        result.setCode(1);
+        return result;
+
+
+
+    }
+
+
+    @RequestMapping("/DelpageeleList")
+    public ResponseInfo UserDel(@RequestBody DelPage delPage){
+        ResponseInfo result = new ResponseInfo();
+        if (!pageService.QueryPageName(delPage.getPage_name())){
+                result.setCode(500);
+        }else {
+            boolean page = pageService.DelpageElements(delPage.getPage_name());
+            if (!page){
+                result.setCode(500);
+                result.setError("删除页面元素失败！");
+            }else {
+                result.setCode(1);
+                result.setMsg("删除成功");
+            }
+        }
+        return result;
+    }
 
 }
